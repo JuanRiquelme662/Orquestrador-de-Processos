b9f44a7ab988a19088ee3f7ec0b0de15044519d6
Juan Riquelme Xavier de Souza <juanriquelmexs2007@gmail.com>
2026-08-22 11:43:10 -0300

    separação da função run

--- main.c
+++ main.c
@@ -18,6 +18,7 @@ void tokenizacao(int *qunt_tokens, char *tokens_sep[], char *entrada) {
     while(token != NULL){
         tokens_sep[*qunt_tokens] = token;
         (*qunt_tokens)++;
+        //eu passo NULL para que ela continue a partir do ponto onde parou na string original
         token = strtok(NULL, " ");
     }
 
@@ -44,35 +45,45 @@ void cadastro_task(int qnt_tokens, char *tokens_sep[], task tasks_cadastradas[],
     printf("Task '%s' cadastrada.\n", nova_task->nome);
 }
 
-void run(char *tokens_sep[], task tasks_cadastradas[], int qnt_tasks) {
-    int encontrou = 0;
+task* buscar_task(char *nome, task tasks_cadastradas[], int qnt_tasks) {
     for (int i = 0; i < qnt_tasks; i++) {
-        if(strcmp(tokens_sep[1], tasks_cadastradas[i].nome) == 0){
-            encontrou =1;
-            pid_t pid = fork();
-            
-            if (pid == 0) {
-                //processo filho
-                //execvp pega o primeiro argumento com o nome do programa a ser executado e o segundo, o argumento completo
-                execvp(tasks_cadastradas[i].args[0], tasks_cadastradas[i].args);
-                //essas linhas so sao rodades caso a linha anterior nao consiga ser executada, porque execvp ja "mata" o processo filho 
-                printf("Erro ao executar task '%s'\n", tasks_cadastradas[i].nome);
-                exit(1);
-            }else if(pid > 0){
-                //processo pai
-                wait(NULL);
-                
-            }else{
-                printf("Erro ao criar processo filho\n");
-            }
-
-            break;
+        if (strcmp(nome, tasks_cadastradas[i].nome) == 0) {
+            return &tasks_cadastradas[i];
         }
     }
-    if (encontrou == 0) {
-        printf("Erro: task '%s' não encontrada\n", tokens_sep[1]);
+    return NULL;
+}
+
+void executar_task(task *t) {
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        //processo filho
+        //execvp pega o primeiro argumento com o nome do programa a ser executado e o segundo, o argumento completo
+        execvp(t->args[0], t->args);
+        //essas linhas so sao rodades caso a linha anterior nao consiga ser executada, porque execvp ja "mata" o processo filho 
+        printf("Erro ao executar task '%s'\n", t->nome);
+        exit(1);
+    } else if (pid > 0) {
+        //processo pai
+        wait(NULL);
+    } else {
+        printf("Erro ao criar processo filho\n");
     }
 }
+
+void run(char *tokens_sep[], task tasks_cadastradas[], int qnt_tasks) {
+    //chama a fucnao buscar_task e retorna a task encontrada para um ponteiro so tipo task
+    task *t = buscar_task(tokens_sep[1], tasks_cadastradas, qnt_tasks);
+
+    if (t == NULL) {
+        printf("Erro: task '%s' não encontrada\n", tokens_sep[1]);
+        return;
+    }
+    //executa a task encontrada atraves do endereco encontrado por buscar_task
+    executar_task(t);
+}
+
 int main(int argc, char *argv[]) {
     task tasks_cadastradas[100];
     int qnt_tasks = 0;
