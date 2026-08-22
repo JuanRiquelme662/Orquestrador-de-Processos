0a2b4d169958257b3c555aab1beab7d2cf970c39
Juan Riquelme Xavier de Souza <juanriquelmexs2007@gmail.com>
2026-08-22 00:55:12 -0300

    adicao da funcao cadastro_task
    criacao de uma funcao com o objetivo de criar uma task

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct task{
+    char nome[100];
+    char *args[100];
+    int qnt_tokens;
+}task;
+
 void tokenizacao(int *qunt_tokens, char *tokens_sep[], char *entrada) {
     char *token = strtok(entrada, " ");
     *qunt_tokens = 0;
@@ -14,13 +20,29 @@ void tokenizacao(int *qunt_tokens, char *tokens_sep[], char *entrada) {
 
 }
 
-void cadastro_taks(){
-    if (num_tokens < 3) {
+void cadastro_taks(int qnt_tokens, char *tokens_sep[], task tasks_cadastradas[], int *qnt_tasks) {
+    if (qnt_tokens < 3) {
         printf("Erro: uso correto é 'task <nome> <programa> [args...]'\n");
         return;
     }
+    task *nova_task  = &tasks_cadastradas[*qnt_tasks];
+    //uso strcpy porque nao tem como fazer nome apontar para tokens_sep porque nome nao e um ponteiro 
+    strcpy(nova_task->nome, tokens_sep[1]);
+    int j = 0;
+    for(int i = 2; i < qnt_tokens; i++){
+        //uso str dup porque so fazer args apontar para o mesmo endereco de tokens_sep vai dar problema quando tokens_sep for sobrescrito
+        nova_task->args[j] = strdup(tokens_sep[i]);
+        j++;
+    }
+    nova_task->args[j] = NULL;
+    nova_task->qnt_tokens = j;
+    (*qnt_tasks)++;
+
+    printf("Task '%s' cadastrada.\n", nova_task->nome);
 }
 int main(int argc, char *argv[]) {
+    task tasks_cadastradas[100];
+    int qnt_tasks = 0;
     char entrada[100];
     char *tokens_sep[100];
     int qnt_tokens = 0;
